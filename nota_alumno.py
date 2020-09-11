3730ca9c97002c8f611d36271e165b1fad470bb2
Osvaldo Ayala <oayala65@gmail.com>
2020-09-11 14:56:21 -0400

    viernes

---
+++ nota_alumno.py
@@ -0,0 +1,15 @@
+lista_aprobado=[]
+lista_reprobdo=[]
+
+s_nota={}
+
+n=int(input('Introduzca la cantidad de alumnos = '))
+
+for i in range(n):
+  nombre=input('Nombre del Alumno = ')
+  nota=int(('input(Ingrese la nota =  )')
+
+  s_nota[nombre]=nota
+
+
+print(alumno_nota)
