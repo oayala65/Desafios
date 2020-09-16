7b58eb4544f789980e0361038cf9ec823d994168
Osvaldo Ayala <oayala65@gmail.com>
2020-09-16 14:43:33 -0400

    M

--- nota_alumno.py
+++ nota_alumno.py
@@ -8,8 +8,6 @@ n=int(input('Introduzca la cantidad de alumnos = '))
 for i in range(n):
   nombre=input('Nombre del Alumno = ')
   nota=int(('input(Ingrese la nota =  )')
-
   s_nota[nombre]=nota
-
-
-print(alumno_nota)
+  
+print(s_nota)
