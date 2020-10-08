8dc2d24e5760819dd60399aea326e9b0e24886b3
Osvaldo Ayala <oayala65@gmail.com>
2020-10-08 15:47:32 -0300

    Con comentarios

--- mipassword_cheq_YO.py
+++ mipassword_cheq_YO.py
@@ -6,7 +6,7 @@ def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
     if res.status_code != 200:
-        raise RuntimeError(f'Error fetching: {res.status.code}, check the API and try again')
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again')
     return res
 
 def get_password_leaks_count(hashes, hash_to_check):
@@ -32,4 +32,4 @@ def main(args):
     return 'done'
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv[1:]))
+    sys.exit(main(sys.argv[1:]))# sys.argv significa que acepta cualquier numero de entradas
